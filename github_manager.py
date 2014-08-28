6c81c80b134a02054fcecb3a9f3fdd2cd51d8c15
Marc Hanheide <marc@hanheide.de>
2014-08-28 14:38:51 +0100

    added package.xml search ability

--- github_manager.py
+++ github_manager.py
@@ -1,4 +1,5 @@
 from github3 import login, user, authorize
+from github3.repos.contents import Contents
 from getpass import getpass
 
 
@@ -7,7 +8,6 @@ class github_manager:
     _gh = None
     __password = None
     _user = None
-
 
     def __init__(self, user=None, token=None):
         if user is not None:
@@ -22,7 +22,6 @@ class github_manager:
             return
         raise Exception('neither user name nor token succeeded')
 
-
     def query_orga_repos(self, organisation, filter='all'):
         org = self._gh.organization(organisation)
         repos = org.iter_repos(filter)
@@ -35,6 +34,38 @@ class github_manager:
             rosinstall.append(entry)
         return rosinstall
 
+    def search_file(self, repo, content, depth, max_depth, fname):
+        res = []
+
+        if isinstance(content, Contents):
+            return []
+        if isinstance(content, dict):
+            for n, c in content.iteritems():
+                if n == fname:
+                    res.append(str(c.path))
+                if c.type == 'dir' and depth < max_depth:
+                    res = res + (self.search_file(repo,
+                                 repo.contents(c.path),
+                                 depth+1, max_depth, fname))
+            return res
+        return []
+
+    def get_package_xmls_from_repo(self, repo, depth):
+        top_level_content = repo.contents('/')
+        return self.search_file(repo, top_level_content, 0, depth, 'package.xml')
+
+    def get_package_xmls(self, organisation, depth):
+        org = self._gh.organization(organisation)
+        repos = org.iter_repos('all')
+
+        res = {}
+        for r in repos:
+            print r.name
+            k = str(r.name)
+            res[k] = self.get_package_xmls_from_repo(r, depth)
+            print res[k]
+        return res
+
     def generate_app_token(self, note = 'github_manager_app', note_url = 'https://github.com/marc-hanheide/ros_gh_mgr', scopes = ['user', 'repo']):
         if self._user is None:
             raise Exception('new tokens can only be generated when logged in using user/passwd credentials')
@@ -44,4 +75,8 @@ class github_manager:
 if __name__ == "__main__":
     ghm = github_manager(user='marc-hanheide')
     ri = ghm.query_orga_repos('strands-project')
-    print ri
+
+    org = ghm._gh.organization('strands-project')
+    repos = org.iter_repos('fork')
+    pxml = ghm.get_package_xmls('strands-project', 1)
+    print pxml
