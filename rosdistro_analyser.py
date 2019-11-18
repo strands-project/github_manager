168cd942faed0f8b9dc3ef77ee4f1f7092c0b434
Marc Hanheide <marc@hanheide.net>
2019-11-18 22:07:36 +0000

    beta

--- rosdistro_analyser.py
+++ rosdistro_analyser.py
@@ -218,7 +218,7 @@ class CacheAnalyser:
                 'deps': set([str(d.name) for d in pkg.build_depends]
                             + [str(d.name) for d in pkg.exec_depends]),
                 'repository': sg.name,
-                'description': sg.description,
+                'description': pkg.description,
                 'authors': [str(a.name) for a in pkg.authors],
                 'maintainers': [str(a.name) for a in pkg.maintainers],
                 'license': ', '.join(pkg.licenses)
@@ -229,8 +229,13 @@ class CacheAnalyser:
 
     def __checkout(self, url, branch, name, dir):
         check_call(["git", "clone", '--depth', '1',
-            '--recurse-submodules',
+            #'--recurse-submodules',
             '-b', branch, url, name], cwd=dir)
+
+    def parse_package_xml(self, package):
+            xml = self._distro.get_release_package_xml(package)
+            root = ET.fromstring(xml)
+            return dictify(root)
 
     def write(self, filename):
         with open(filename, 'w') as f:
@@ -250,10 +255,11 @@ class CacheAnalyser:
         dot = pgv.AGraph(label="<<B>Dependency Graph of Repositories</B>>",
                          directed=True,
                          strict=True,
-                         splines='ortho',
+                         size=16,
+                         splines='true',
                          compound=True,
-                         ratio='compress',
-                         rankdir='LR',
+                         ratio='expand',
+                         rankdir='tb',
                          concentrate=True)
         released = dot.add_subgraph(name='released')
         non_released = dot.add_subgraph(name='non_released')
@@ -312,7 +318,7 @@ class CacheAnalyser:
                     weight = 1
 
                 dot.add_edge(
-                    dr, repo_name, color=edge_color, weight=weight, penwidth=2,
+                    repo_name, dr, color=edge_color, weight=weight, penwidth=2,
                     constraint=True,
                     URL=self._repositories[dr]['url'],
                     edgetooltip=('"%s" requires:\n  %s' % (repo_name, edge_tooltip)))
@@ -361,17 +367,37 @@ class CacheAnalyser:
 
     def generate_md_repo(self, repo_name):
         repo = self._repositories[repo_name]
-        str = '---\n\n# [%s](%s)\n' % (repo_name, repo['url'])
-        str += 'Source Code: %s (branch: %s)\n\n' % (
-            repo['url'], repo['version']
-        )
+        str = '---\n\n# %s\n' % repo_name
+
+        if repo['type'] == 'git':
+            if repo['jenkins_job']:
+                str += 'Source Code: %s (branch: `%s`) [![buildStatus](%s/badge/icon)](%s)\n\n' % (
+                    repo['url'], repo['version'], repo['jenkins_job'], repo['jenkins_job']
+                )
+            else:
+                str += 'Source Code: %s (branch: `%s`)\n\n' % (
+                    repo['url'], repo['version']
+                )
 
         str += '\n__`rosinstall` definition:__\n'
-        str += '\n```\n%s```\n' % (
+        str += '\n```\n%s' % (
             self.generate_rosinstall(repo_name)
         )
-        str += '\n__included packages:__\n\n'
+        if repo['requires_repositories']:
+            for d in repo['requires_repositories']:
+                if self._repositories[d]['status'] != 'released':
+                    str += '%s' % (
+                        self.generate_rosinstall(d)
+                    )
+        str += '```\n'
 
+        if repo['requires_repositories']:
+            str += '\n__depends on the following repositories:__\n\n'
+            str += ', '.join(['[`%s`](#%s)\n' % (d, d) for d in repo['requires_repositories']])
+        str += '\n\n'
+
+
+        str += '\n__included packages:__\n\n'
         tablehead = '| package | maintainer | authors | licence |\n'
         tableline = '| ------- | ---------- | ------- | ------- |\n'
         str += tablehead + tableline
@@ -422,187 +448,6 @@ class CacheAnalyser:
             outstr += self.generate_md_repo(repo)
 
         return outstr
-
-
-####################
-
-    def parse_package_xml(self, package):
-        xml = self._distro.get_release_package_xml(package)
-        root = ET.fromstring(xml)
-        return dictify(root)
-
-    def clean_out(self):
-        for rd in self._repo_deps:
-            self._repo_deps[rd] = self._repo_deps[rd].intersection(self._repos)
-            if rd in self._repo_deps[rd]:
-                self._repo_deps[rd].remove(rd)
-
-    def repo_collect(self):
-        d = {}
-        for r in self._repos:
-
-            d[r] = {
-                'deps': self._repo_deps[r],
-                'name': r,
-                'url': self._distro.repositories[r].source_repository.url,
-                'version':
-                    self._distro.repositories[r].source_repository.version,
-                'packages_depended_on': self._repos[r],
-                'packages': {p: self._pkgs[p] for p in self._repos[r]},
-                'contained_packages':
-                    self._distro.repositories[r]
-                    .release_repository.package_names
-            }
-        return d
-
-    def analyse_pkg(self, pkg_names=[]):
-        if not pkg_names:
-            pkg_names = set(self._distribution.release_packages)
-        self._roots = pkg_names
-        for p in pkg_names:
-            self._analyse_pkg(p)
-        self.clean_out()
-        self.repos = self.repo_collect()
-
-        #self.repo_collect()
-
-
-    def _analyse_pkg(self, pkg_name, depth=0):
-        if depth > self._max_depth:
-            return
-        deps = get_recursive_dependencies(self._distro,
-                                          [pkg_name], limit_depth=1)
-        pkg = self._distro.release_packages[pkg_name]
-        repo = self._distro.repositories[pkg.repository_name]
-        s = {
-            'depth': depth,
-            'depends': list(deps),
-            'name': pkg_name
-        }
-
-        if repo.source_repository:
-            s['source'] = {
-                #'repo': repo.source_repository,
-                'name': repo.source_repository.name,
-                'url': repo.source_repository.url,
-                'orga': repo.source_repository.url.split('/')[3],
-                'branch': repo.source_repository.version
-            }
-            if s['source']['orga'].lower() in self._orgas:
-                self._orga_url[s['source']['orga'].lower()] =\
-                    '/'.join(
-                        repo.source_repository.url.split('/')[:4]
-                    )
-                if repo.release_repository:
-                    s['release'] = {
-                    #    'repo': repo.release_repository,
-                        'name': repo.release_repository.name,
-                        'url': repo.release_repository.url,
-                        'version': repo.release_repository.version
-                    }
-                s['package_xml'] = self.parse_package_xml(pkg_name)
-                px = s['package_xml']['package']
-                s['package'] = {
-                    'authors': ([a['_text']
-                                 for a in px['author']]
-                                if 'author' in px
-                                else ''),
-                    'maintainers': ([a['_text']
-                                     for a in px['maintainer']]
-                                    if 'maintainer' in px
-                                    else ''),
-                    'description': (' '.join(
-                                    [a['_text']
-                                     for a in px['description']])
-                                    if 'description' in px
-                                    else ''),
-                    'license': (' '.join(
-                                    [a['_text']
-                                     for a in px['license']])
-                                if 'license' in px
-                                else '')
-                }
-                self._repos[repo.name].add(pkg_name)
-                self._pkg2repo[pkg_name] = repo.name
-
-                self._pkgs[pkg_name] = s
-                for p in deps:
-                    d_pkg = self._distro.release_packages[p]
-                    d_repo = self._distro.repositories[d_pkg.repository_name]
-                    self._repo_deps[repo.source_repository.name].add(d_repo.name)
-                    self._analyse_pkg(p, depth + 1)
-
-    def analyse_repo(self, repo):
-        pprint(self._distro.repositories[repo].source_repository.__dict__)
-        pprint(self._distro.repositories[repo].release_repository.__dict__)
-
-
-    def analyse(self):
-        released_names, unreleased_names = get_package_names(self._distro)
-        pprint(
-            unreleased_names
-            #get_recursive_dependencies(self._distro, ['strands_apps'], source=True)
-        )
-
-
-    def generate_repo_requires_graph(self):
-        dot = pgv.AGraph(label="<<B>Dependency Graph of Repositories</B>>",
-                         directed=True,
-                         strict=True,
-                         splines='True',
-                         compound=True,
-                         rankdir='TB',
-                         concentrate=False)
-
-        for k, sg in self.repos.items():
-            pstr = ''
-            for pname, pkg in sg['packages'].items():
-                # pstr += '  <I>%s </I>(%s)<BR ALIGN="LEFT"/>' % (
-                #     pname, ', '.join(pkg['package']['maintainers']))
-                pstr += '  <I>%s</I><BR ALIGN="LEFT"/>' % (
-                    pname)
-            pstr += ''
-            # maintainers = ''
-            # for pn in sg['packages']:
-            #     p = sg['packages']
-            #     for m in p['maintainers']:
-            #         maintainers += '  ' + pn + ': ' + m + '<br align="left"/>'
-            #     if p['release_status'] is None:
-            #         nc = 'red'
-            #     else:
-            #         if p['release_status'] == 'release':
-            #             nc = 'green'
-            #         else:
-            #             nc = 'yellow'
-            #dot.add_node(k, label='<<I>'+k.upper()+'</I><br align="left"/>'+maintainers+'>', color=nc)
-            label = ('<<B>%s</B><BR ALIGN="LEFT"/>'
-                        '<FONT POINT-SIZE="8">%s</FONT>>' %
-                        (k.lower(), pstr))
-
-            dot.add_node(k,
-                         label=label)
-
-            for dr in sg['deps']:
-                dot.add_edge(k, dr, weight=10, constraint=True)
-
-        # for pname, pkg in self._pkgs.items():
-        #     for dpkg in pkg['depends']:
-        #         if dpkg in self._pkgs:
-        #             #dot.node(dpkg)
-        #             #nodes.append(dpkg)
-        #             repo1 = pkg['repo']
-        #             repo2 = self._pkgs[dpkg]['repo']
-        #             rs = self._pkgs[dpkg]['release_status']
-        #             if not repo1 == repo2:
-        #                 if rs is None:
-        #                     ec = 'red'
-        #                 else:
-        #                     if rs == 'release':
-        #                         ec = 'green'
-        #                     else:
-        #                         ec = 'yellow'
-        #                 dot.add_edge(repo1, repo2, weight=10, constraint=True, color=ec)
-        return dot
 
     def preamble(self):
         return ('This is an overview of repositories and packages '
@@ -672,20 +517,6 @@ def main():
     print(ca.generate_markdown_repos())
     #ca.analyse_non_released_repos()
     return
-    #ca.analyse('strands_apps')
-    #print(get_package_names(ca._distro))
-    ca.analyse_pkg(_roots)
-
-    dot = ca.generate_repo_requires_graph()
-    dot.layout(prog='dot')
-    dot.draw('repos-%s.png' % args.distro)
-    dot.draw('repos-%s.pdf' % args.distro)
-
-    print(ca.preamble())
-    print(ca.generate_markdown_repos())
-    #pprint(ca._pkgs)
-    #pprint(dict(ca._repo_deps))
-    #pprint(dict(ca._repos))
 
 if __name__ == "__main__":
     basicConfig(level=INFO)
